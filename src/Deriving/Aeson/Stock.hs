214232e545ecce4473ea310cbb5fc0d61c7c2d27
Poscat <poscat@mail.poscat.moe>
2020-03-19 18:49:44 +0800

    Add aliases for stripping strictly prefiex strings

--- src/Deriving/Aeson/Stock.hs
+++ src/Deriving/Aeson/Stock.hs
@@ -4,6 +4,8 @@
 module Deriving.Aeson.Stock
   ( Prefixed
   , PrefixedSnake
+  , Prefixed'
+  , PrefixedSnake'
   , Snake
   , Vanilla
   -- * Reexports
@@ -17,8 +19,14 @@ import Deriving.Aeson
 -- | Field names are prefixed by @str@; strip them from JSON representation
 type Prefixed str = CustomJSON '[FieldLabelModifier (StripPrefix str)]
 
+-- | Field names are strictly prefixed by @str@; strip them from JSON representation
+type Prefixed' str = CustomJSON '[FieldLabelModifier (StripStrictPrefix str)]
+
 -- | Strip @str@ prefices and convert from CamelCase to snake_case
 type PrefixedSnake str = CustomJSON '[FieldLabelModifier (StripPrefix str, CamelToSnake)]
+
+-- | Strip @str@ strict prefixes and convert from CamelCase to snake_case
+type PrefixedSnake' str = CustomJSON '[FieldLabelModifier (StripStrictPrefix str, CamelToSnake)]
 
 -- | Convert from CamelCase to snake_case
 type Snake = CustomJSON '[FieldLabelModifier CamelToSnake]
