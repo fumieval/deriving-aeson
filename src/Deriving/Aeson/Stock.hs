6ba33361c002b3eb877a102feb44d3b2d9986998
Poscat <poscat@mail.poscat.moe>
2020-03-19 18:38:30 +0800

    Fix typo

--- src/Deriving/Aeson/Stock.hs
+++ src/Deriving/Aeson/Stock.hs
@@ -17,7 +17,7 @@ import Deriving.Aeson
 -- | Field names are prefixed by @str@; strip them from JSON representation
 type Prefixed str = CustomJSON '[FieldLabelModifier (StripPrefix str)]
 
--- | Strip @str@ prefices and convert from CamelCase to snake_case
+-- | Strip @str@ prefixes and convert from CamelCase to snake_case
 type PrefixedSnake str = CustomJSON '[FieldLabelModifier (StripPrefix str, CamelToSnake)]
 
 -- | Convert from CamelCase to snake_case
