314e5de2b3118f86852d9727998d0bf22a5a2140
Flavio Corpa <flaviocorpa@gmail.com>
2024-11-22 14:35:37 +0100

    Add StripSuffix modifier

--- src/Deriving/Aeson/Stock.hs
+++ src/Deriving/Aeson/Stock.hs
@@ -4,6 +4,8 @@
 module Deriving.Aeson.Stock
   ( Prefixed
   , PrefixedSnake
+  , Suffixed
+  , SuffixedSnake
   , Snake
   , Vanilla
   -- * Reexports
@@ -20,6 +22,12 @@ type Prefixed str = CustomJSON '[FieldLabelModifier (StripPrefix str)]
 
 -- | Strip @str@ prefices and convert from CamelCase to snake_case
 type PrefixedSnake str = CustomJSON '[FieldLabelModifier '[StripPrefix str, CamelToSnake]]
+
+-- | Field names are suffixed by @str@; strip them from JSON representation
+type Suffixed str = CustomJSON '[FieldLabelModifier (StripSuffix str)]
+
+-- | Strip @str@ suffixes and convert from CamelCase to snake_case
+type SuffixedSnake str = CustomJSON '[FieldLabelModifier '[StripSuffix str, CamelToSnake]]
 
 -- | Convert from CamelCase to snake_case
 type Snake = CustomJSON '[FieldLabelModifier CamelToSnake]
